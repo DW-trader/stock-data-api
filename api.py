7965ad4d51527fff290b7a4eb944aaf1b2fd2643
Vedran <vedran.vukelic@gmail.com>
2017-05-27 14:17:53 +0200

    Proof of concept.

--- api.py
+++ api.py
@@ -1,5 +1,20 @@
 
+import simplejson as json
 from flask import Flask
+from influxdb import InfluxDBClient
+
+
+def _get_data(symbol, timestamp):
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'test')
+    query_str = 'select * from daily_stock_data where symbol = \'{0}\' and time > \'{1}\''.format(symbol, timestamp)
+
+    points = client.query(query_str).get_points()
+
+    result = []
+    for point in points:
+        result.append(point)
+
+    return result
 
 
 app = Flask(__name__)
@@ -7,7 +22,8 @@ app = Flask(__name__)
 
 @app.route('/query/<symbol>/<timestamp>', methods=['GET'])
 def get_data(symbol, timestamp):
-    return '{0} {1}'.format(symbol, timestamp)
+    data = _get_data(symbol, timestamp)
+    return json.dumps(data)
 
 
 if __name__ == '__main__':
